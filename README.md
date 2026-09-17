16c24ac81a6eb9238e7c4705ec0404c982af66c2
Catherine <cath.thedim@gmail.com>
2026-09-17 16:24:00 -0300

    feature: novos endpoints

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # MyBookList
 
-Rede social para salvar livros.
+Rede social para listar, salvar e descobrir livros.
 
 ## Rodar a aplicação
 
 Após fazer download do projeto, somente é necessário abrir o arquivo **index.html** no navegador de preferência.
+
+### Rodar pelo Docker
+
+É necessário ter o [Docker](https://docs.docker.com/engine/install/) instalado e em execução.
+
+Abrir o terminal como administrador no diretório que possui o arquivo **Dockerfile** e criar a imagem:
+
+#### `docker build -t mybooklist-front .`
+
+Em seguida, para rodar o projeto, execute o comando:
+
+#### `docker run -p 5000:5000 mybooklist-front`
+
+Para abrir a aplicação, basta acessar o link [http://localhost:5000/](http://localhost:5000/) no navegador.
+
+## Fluxograma do projeto
+
+![Fluxograma indicando componentes do projeto](./img/Fluxograma.png)
--- scripts.js
+++ scripts.js
@@ -1,15 +1,37 @@
 window.onload = listarLivros();
 const divLivros = document.getElementById('livros');
-formulario = document.getElementById("formulario");
+const formularioAdicionar = document.getElementById("formularioAdicionar");
+const formularioBuscaAvancada = document.getElementById("formularioBuscaAvancada");
+const list = document.querySelector("#livros");
+const prevButton = document.querySelector("#prev");
+const nextButton = document.querySelector("#next");
+const pageNumberValue = document.querySelector("#page-number")
+
+let startIndex = 0;
+let endIndex = 10;
+let pageNumber = 0;
+
+pageNumberValue.value = pageNumber
 
 function mostraAdicionarLivro() {
-  divEscondida = document.getElementById('div-adicionar');
+  divAdicionar = document.getElementById('div-adicionar');
   
-  if (divEscondida.style.display == "none" || divEscondida.style.display == "") {
-    divEscondida.style.display = "block";
+  if (divAdicionar.style.display == "none" || divAdicionar.style.display == "") {
+    divAdicionar.style.display = "block";
   }
   else {
-    divEscondida.style.display = "none";
+    divAdicionar.style.display = "none";
+  }
+}
+
+function mostraBuscaAvancada() {
+  divBuscaAvancada = document.getElementById('div-busca-avancada');
+  
+  if (divBuscaAvancada.style.display == "none" || divBuscaAvancada.style.display == "") {
+    divBuscaAvancada.style.display = "block";
+  }
+  else {
+    divBuscaAvancada.style.display = "none";
   }
 }
 
@@ -50,10 +72,11 @@ async function listarLivros() {
         divLivros.innerHTML += 
             "<li class=\"livro\">"
           + "   <img class=\"capa\" src=\"" + listaLivros[i].capa + "\" alt=\"Capa do livro "+ listaLivros[i].nome +"\">"
-          + "   <p class=\"titulo\">" + listaLivros[i].nome + "</p>"
-          + "   <p class=\"autor\">" + listaLivros[i].autor + "</p>"
+          + "   <p class=\"titulo\"" + (listaLivros[i].nome.length > 22 ? "style=\"font-size:11px\"" : "") + ">" + listaLivros[i].nome + "</p>"
+          + "   <p class=\"autor\"" + (listaLivros[i].autor.length > 22 ? "style=\"font-size:11px\"" : "") + ">" + listaLivros[i].autor + "</p>"
           + "   <p class=\"ano_publicacao\">" + listaLivros[i].ano_publicacao + "</p>"
-          + "   <img class=\"lixeira\" src=\"img/delete.png\" alt=\"Deletar livro\" onClick=\"deletarLivro('" + listaLivros[i].nome.replace("'", "\\'") + "')\">"
+          + "   <img class=\"icone\" src=\"img/edit.png\" alt=\"Editar livro\" onClick=\"editarLivro('" + listaLivros[i].nome.replace("'", "\\'") + "')\">"
+          + "   <img class=\"icone\" src=\"img/delete.png\" alt=\"Deletar livro\" onClick=\"deletarLivro('" + listaLivros[i].nome.replace("'", "\\'") + "')\">"
           + "</li>"
       }
     }
@@ -93,10 +116,10 @@ async function buscarLivro() {
         divLivros.innerHTML += 
             "<li class=\"livro\">"
           + "   <img class=\"capa\" src=\"" + listaLivros[i].capa + "\" alt=\"Capa do livro "+ listaLivros[i].nome +"\">"
-          + "   <p class=\"titulo\">" + listaLivros[i].nome + "</p>"
-          + "   <p class=\"autor\">" + listaLivros[i].autor + "</p>"
+          + "   <p class=\"titulo\"" + (listaLivros[i].nome.length > 22 ? "style=\"font-size:11px\"" : "") + ">" + listaLivros[i].nome + "</p>"
+          + "   <p class=\"autor\"" + (listaLivros[i].autor.length > 22 ? "style=\"font-size:11px\"" : "") + ">" + listaLivros[i].autor + "</p>"
           + "   <p class=\"ano_publicacao\">" + listaLivros[i].ano_publicacao + "</p>"
-          + "   <img class=\"lixeira\" src=\"img/delete.png\" alt=\"Deletar livro\" onClick=\"deletarLivro('" + listaLivros[i].nome.replace("'", "\\'") + "')\">"
+          + "   <img class=\"icone\" src=\"img/delete.png\" alt=\"Deletar livro\" onClick=\"deletarLivro('" + listaLivros[i].nome.replace("'", "\\'") + "')\">"
           + "</li>"
       }
     }
@@ -131,7 +154,7 @@ function recarregarLivros() {
   }, 1000);
 }
 
-formulario.addEventListener("submit", function(e) {
+formularioAdicionar.addEventListener("submit", function(e) {
   e.preventDefault();
 
   const dados = new FormData();
@@ -140,6 +163,10 @@ formulario.addEventListener("submit", function(e) {
   dados.append('ano_publicacao', document.getElementById("ano_publicacao").value);
   dados.append('capa', document.getElementById("capa").value);
   
+  adicionarLivro(dados);
+})
+
+function adicionarLivro(dados) {
   let url = 'http://127.0.0.1:5000/adicionar_livro';
   fetch(url, {
     method: 'post',
@@ -148,7 +175,7 @@ formulario.addEventListener("submit", function(e) {
     .then(async res => {
       if (res.ok) {
         alert("Livro '"+dados.get("nome")+"' adicionado com sucesso!");
-        formulario.reset();
+        formularioAdicionar.reset();
       }
       else {
         const erro = await res.text(); 
@@ -165,4 +192,148 @@ formulario.addEventListener("submit", function(e) {
     });
 
     recarregarLivros();
+}
+
+formularioBuscaAvancada.addEventListener("submit", async function(e) {
+  e.preventDefault();
+
+  const formulario = document.getElementById("formularioBuscaAvancada");
+  const formData = new FormData(formulario);
+  const tipoSelecionado = formData.get("tipo-busca");
+
+  const params = new URLSearchParams({
+      busca: formData.get("busca")
+  });
+
+  let url = `http://127.0.0.1:5000/`;
+
+  if (tipoSelecionado == "Nome") url += `buscar_nome?${params}`;
+  else if (tipoSelecionado == "Autor") url += `buscar_autor?${params}`;
+  else url += `buscar_geral?${params}`;
+
+  divLivros.innerHTML = '<img id="loading" src="img/loading.gif">';
+
+  const resposta = await fetch(url, {
+    method: 'get',
+    body: null
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Erro: ', error);
+    });
+
+    setTimeout(function() {
+      tratamentoRespostaAPI(resposta);
+    }, 1000);
 })
+
+function tratamentoRespostaAPI(resposta) {
+  divLivros.innerHTML = '';
+
+  if (resposta['docs'].length == 0) {
+    divLivros.innerHTML = '<p>Nenhum livro encontrado!</p>';
+  }
+  else {
+    for (i = 0; i < resposta['docs'].length; i++) {
+      if (i != 0 && i%6 == 0) {
+          divLivros.innerHTML += "<br>"
+        }
+          
+        divLivros.innerHTML += 
+            "<li class=\"livro\" id=\"resposta['docs'][i]['cover_edition_key']\">"
+          + "   <img class=\"capa\" src=\"https://covers.openlibrary.org/b/id/" + resposta['docs'][i]['cover_i'] + "-L.jpg\" alt=\"Capa do livro "+ resposta['docs'][i]['title'] +"\">"
+          + "   <p class=\"titulo\"" + (resposta['docs'][i]['title'].length > 22 ? "style=\"font-size:11px\"" : "") + ">" + resposta['docs'][i]['title'] + "</p>"
+          + "   <p class=\"autor\"" + (resposta['docs'][i]['author_name'].length > 20 ? "style=\"font-size:11px\"" : "") + ">" + resposta['docs'][i]['author_name'] + "</p>"
+          + "   <p class=\"ano_publicacao\">" + resposta['docs'][i]['first_publish_year'] + "</p>"
+          + "   <img class=\"icone\" src=\"img/add.png\" alt=\"Adicionar\" onClick=\"adicionarAColecao('" + resposta['docs'][i]['title'] + "', '" + resposta['docs'][i]['author_name'] + "', " + resposta['docs'][i]['first_publish_year'] + ", 'https://covers.openlibrary.org/b/id/" + resposta['docs'][i]['cover_i'] + "-L.jpg')\">"
+          + "   <img class=\"icone\" src=\"img/info.png\" alt=\"Informações adicionais\" onClick=\"infosLivro('" + resposta['docs'][i]['cover_edition_key'] + "')\">"
+          + "</li>"
+    }
+  }
+  //paginar(resposta['docs']);
+}
+
+function adicionarAColecao(nome, autor, ano_publicacao, capa) {
+  if (confirm("Deseja adicionar o livro '" + nome + "' à coleção?")) {
+    const dados = new FormData();
+    dados.append('nome', nome);
+    dados.append('autor', autor);
+    dados.append('ano_publicacao', ano_publicacao);
+    dados.append('capa', capa);
+    
+    adicionarLivro(dados);
+  }
+}
+
+async function infosLivro(ol_id) {
+  const params = new URLSearchParams({
+      busca: ol_id
+  });
+  
+  let url = `http://127.0.0.1:5000/buscar_especifica?${params}`;
+
+  const resposta = await fetch(url, {
+    method: 'get',
+    body: null
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Erro: ', error);
+    });
+
+    alert('= Informações adicionais =\n\nPáginas: '+resposta['number_of_pages']+'\nEditora: '+resposta['publishers']);
+}
+
+/*
+prevButton.addEventListener("click", () => {
+  if (endIndex < 20) {
+    startIndex = 0;
+    endIndex = 10;
+  } else {
+    startIndex -= 10;
+    endIndex -= 10;
+    pageNumber -= 1;
+  }
+  pageNumberValue.value = pageNumber;
+  paginar();
+});
+
+nextButton.addEventListener("click", () => {
+  if (endIndex < respostas.length) {
+    startIndex += 10;
+    endIndex += 10;
+    pageNumber += 1;
+  }
+  pageNumberValue.value = pageNumber;
+  paginar();
+});
+
+pageNumberValue.addEventListener("change",(e) => {
+  let currentPageNumber = Number.parseInt(e.target.value)
+  let maxPageNumber = Math.floor(respostas.length/10)
+  if(currentPageNumber > maxPageNumber){
+   currentPageNumber = maxPageNumber;
+    e.target.value = value
+  }
+  else if(currentPageNumber < 0){
+    currentPageNumber = 0;
+    e.target.value = value
+  }
+   startIndex = currentPageNumber * 10;
+   endIndex = startIndex + 10
+   pageNumber = currentPageNumber
+   paginar();
+})
+
+const paginar = (respostas) => {
+  console.log("páginar")
+  const paginacao = respostas
+    .slice(startIndex, endIndex)
+    .map((row) => {
+      return `<li>${row.name}</li>`;
+    })
+    .join("");
+
+  list.innerHTML = paginacao;
+}
+//paginacao();*/
